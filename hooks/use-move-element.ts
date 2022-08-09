a8f122058ee0a3cfe97786ee76c3a2f2aff1ee2a
Ismael Flores <bayzzer@outlook.com>
2022-08-08 21:46:19 -0500

    fix: useMoveElement

--- hooks/use-move-element.ts
+++ hooks/use-move-element.ts
@@ -1,27 +1,27 @@
 import { useEventListener } from '@bayzzer/hooks'
 import { RefObject } from 'react'
 
-export const useMoveElement = <T extends HTMLElement>(elementRef?: RefObject<T>) => {    
+export const useMoveElement = <T extends HTMLElement>(elementRef?: RefObject<T>) => {
     var x = 0,
         y = 0,
         mousedown = false
 
     useEventListener('mousedown', (event) => {
-        if(!elementRef) return  
-        mousedown = true
-        var element = event.target as HTMLElement
+        if (!elementRef) return
+        var element = elementRef.current as HTMLElement
         x = element.offsetLeft - event.clientX
         y = element.offsetTop - event.clientY
+        mousedown = true
     }, elementRef!)
 
-    useEventListener('mouseup', () => {        
+    useEventListener('mouseup', () => {
         mousedown = false
     }, elementRef!)
 
     useEventListener('mousemove', (event) => {
-        if(!elementRef) return 
+        if (!elementRef) return
         if (mousedown) {
-            var element = event.target as HTMLElement
+            var element = elementRef.current as HTMLElement
             element.style.position = "absolute"
             element.style.left = `${event.clientX + x}px`
             element.style.top = `${event.clientY + y}px`
