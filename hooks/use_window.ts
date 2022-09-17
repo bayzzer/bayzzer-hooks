34496db276b87b6076824cbc0649a0367534e8d8
Ismael Flores <bayzzer@outlook.com>
2022-09-16 21:01:49 -0500

    fix: add useSessionStorage ...etc

--- hooks/use_window.ts
+++ hooks/use_window.ts
@@ -1,10 +1,38 @@
-export const useWindow = () => {
-    const isDOM =  typeof window !== 'undefined' &&
-    window.document &&
-    window.document.documentElement
-    
-    return {
+import { useState } from "react"
+import { useEventListener, useIsomorphicLayoutEffect } from "."
+interface WindowProperties {
+    width: number
+    height: number
+    isClient: boolean | HTMLElement
+    isServer: boolean | HTMLElement
+}
+
+export function useWindowSize(): WindowProperties {
+
+    const isDOM = typeof window !== 'undefined' &&
+        window.document &&
+        window.document.documentElement
+
+    const [windowProperties, setWindowProperties] = useState<WindowProperties>({
+        width: 0,
+        height: 0,
         isClient: isDOM,
         isServer: !isDOM,
+    })
+
+    const handleSize = () => {
+        setWindowProperties({
+            ...windowProperties,
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
     }
+
+    useEventListener('resize', handleSize)
+    
+    useIsomorphicLayoutEffect(() => {
+        handleSize()
+    }, [])
+
+    return windowProperties
 }
